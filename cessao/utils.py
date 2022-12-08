d450b25fc6b8545d5a51dcd5fa42f8b130ca4b25
Felipe <felipecmc03@gmail.com>
2022-12-08 10:52:30 -0300

    fix/variaveis de ambiente

--- cessao/utils.py
+++ cessao/utils.py
@@ -1,12 +1,18 @@
 import pandas as pd
 import boto3
+import os
 
 def readCSV(data):
     
     bucket = data['bucket_name']
     file_name = data['object_key']
 
-    s3 = boto3.client('s3') 
+    s3 = boto3.client(
+        's3',     
+        aws_access_key_id=os.environ.ACCESS_KEY,
+        aws_secret_access_key=os.environ.SECRET_KEY,
+        aws_session_token=os.environ.SESSION_TOKEN
+    ) 
 
     obj = s3.get_object(Bucket= bucket, Key= file_name)
 
